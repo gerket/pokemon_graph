354233916095003390eaacd4e9c3ab521a4eda7d
Tom Gerke <tomgerke10@gmail.com>
2019-02-11 16:20:11 -0500

    Changes to the app.py

--- app.py
+++ app.py
@@ -3,25 +3,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
-############# Make changes here
-
 
 app = dash.Dash()
 server = app.server
 
+############# Make changes here
+
 app.layout = html.Div(children=[
-    html.H1('Plotly Dash - the best way to visualize your data!'),
+    html.H1('This is a better title!'),
     dcc.Graph(
         id='this_is_an_id',
         figure={
             'data': [
-                {'x': ['Dash', 'Powerpoint', 'Lascaux cave paintings'], 'y': [8, 2, 3], 'type': 'bar', 'name': 'Intelligence'},
-                {'x': ['Dash', 'Powerpoint', 'Lascaux cave paintings'], 'y': [7, 1, 5], 'type': 'bar', 'name': 'Beauty'},
+                {'x': ['Dog', 'Cat', 'Lobster'], 'y': [7, 8, 2], 'type': 'bar', 'name': 'Intelligence'},
+                {'x': ['Dog', 'Cat', 'Lobster'], 'y': [7, 3, 2], 'type': 'bar', 'name': 'Weight'},
             ],
             'layout': {
-                'title': "Because friends don't let friends use Microsoft Powerpoint",
-                'xaxis':{'title':'Choice of data visualization'},
-                'yaxis':{'title':'Approval rating by average data scientist'},
+                'title': "Animal Comparison",
+                'xaxis':{'title':'Animal'},
+                'yaxis':{'title':'Completely science-backed numbers with no metric'},
             }
         }
     )]
