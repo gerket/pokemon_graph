c51f190261cd887ead2c9d7ae8705f8c33a27b73
Tom Gerke <tomgerke10@gmail.com>
2019-02-13 14:53:08 -0500

    added comments, deleted unnecessary code

--- app.py
+++ app.py
@@ -10,69 +10,59 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+
 ############# Make changes here
 
 
+
+
+# Setting up the initial api url
 url_parameters = dict(
     base_url = "pokeapi.co",
     directory = "api/v2/pokemon",
     pokemon_id = ''
 )
 
-def reset_endpoint():
-    global endpoint
-    endpoint = "http://{base_url}/{directory}/{pokemon_id}".format(**url_parameters)
-
 endpoint  = "http://{base_url}/{directory}/{pokemon_id}".format(**url_parameters)
 
-temp_results = requests.get(endpoint)
+# Getting a result from the api that includes the number of pokemon
+first_request = requests.get(endpoint)
+first_results = first_request.json()
 
-temp_results = temp_results.json()
-temp_results = requests.get(endpoint + '?offset=0&limit='+str(temp_results['count']))
-temp_results = temp_results.json()
+# Getting a result containing all of the pokemon's names and api urls
+second_request = requests.get(endpoint + '?offset=0&limit='+str(first_results['count']))
+second_results = second_request.json()
 
+# Storing the Pokemon names and urls for later use
 name_url = dict()
-for i in temp_results['results']:
+for i in second_results['results']:
     name_url[i['name']]=i['url']
 
-# multi_select_options = []
-# for i in temp_results['results']:
-#     multi_select_options.append(
-#     {
-#         'label':i['name'].capitalize(),
-#         'value':i['name']
-#     })
-
+#
 app.layout = html.Div([
-        # multi-select dropdown menu
+
         html.Div([
+            # Big header
             html.H1('Comparing Pokemon Statistics!'),
+            # Little bit smaller header
             html.H2('Choose Pokemon to compare:'),
+
+            # multi-select dropdown menu
             dcc.Dropdown(
                 id='pokemon_choices',
-                options=[{'label':i['name'].capitalize(), 'value':i['name']} for i in temp_results['results']],
+                options=[{'label':i['name'].capitalize(), 'value':i['name']} for i in second_results['results']],
                 value='bulbasaur',
                 multi=True
             ),
         ]),
-        # Graphs
-        html.H2('Comparison Graphs:'),
-        dcc.Graph(
-            id='graph_fig',
-            #figure=go.Figure()
-            # figure={
-            #     'data': [
-            #         {'x': ['Dog', 'Cat', 'Lobster'], 'y': [7, 8, 2], 'type': 'bar', 'name': 'Intelligence'},
-            #         {'x': ['Dog', 'Cat', 'Lobster'], 'y': [7, 3, 2], 'type': 'bar', 'name': 'Weight'},
-            #     ],
-            #     'layout': {
-            #         'title': "Animal Comparison",
-            #         'xaxis':{'title':'Animal'},
-            #         'yaxis':{'title':'Completely science-backed numbers with no metric'},
-            #     }
-            # }
-        )
-
+        html.Div([
+            # Little bit smaller header
+            html.H2('Comparison Graphs:'),
+            # Graph placement
+            dcc .Graph(
+                id='graph_fig',
+                )
+        ])
     ]
 )
 
@@ -83,15 +73,6 @@ def update_graph(input_pokemon_choices):
     if type(input_pokemon_choices)==str:
         input_pokemon_choices = [input_pokemon_choices]
 
-    # if input_pokemon_choices == []:
-    #     return {
-    #         ‘data’: [
-    #             {   ‘x’: 'None',
-    #                 ‘y’: 0,
-    #                 ‘type’: ‘bar’,
-    #                 ‘name’: 'Test'
-    #             }],
-    #         ‘layout’: { ‘title’: “Health” } }
     pokedex = dict()
 
     #api calls to get the relevent info for each pokemon
@@ -100,17 +81,17 @@ def update_graph(input_pokemon_choices):
         temp_results = temp_results.json()
 
         temp_poke_df = pd.DataFrame(temp_results['stats'])
-        temp_poke_df['stat_name'] = [x['name'].capitalize() for x in temp_poke_df['stat']] #capitalize here
+        temp_poke_df['stat_name'] = [x['name'].capitalize() for x in temp_poke_df['stat']]
         temp_poke_df.drop(columns=['stat', 'effort'], inplace=True)
 
-        pokedex[poke_name] = {'name':temp_results['name'],'df':temp_poke_df, 'id':temp_results['id']}
+        pokedex[poke_name] = temp_poke_df
 
     # Converting dict of stats into dataframe
     pd_pokedex = pd.DataFrame()
-    for n, v in pokedex.items():
-        dat = [list(v['df']['base_stat'])]
-        dat[0].append(n.capitalize()) #capitalize here
-        cols = list(v['df']['stat_name'])
+    for name, value in pokedex.items():
+        dat = [list(value['base_stat'])]
+        dat[0].append(name.capitalize())
+        cols = list(value['stat_name'])
         cols.append('Name')
         pd_pokedex = pd_pokedex.append(
             pd.DataFrame(
@@ -119,28 +100,26 @@ def update_graph(input_pokemon_choices):
         )
 
 
-    traces = []
-    for col in [x for x in pd_pokedex.columns if x != 'Name']:
-        traces.append(
-            go.Bar({
-                'x':list(pd_pokedex['Name']),
-                'y':list(pd_pokedex[col]),
-                'name':col
-            })
-        )
+    # List comprehension to create data for the graph
+    traces = [
+        go.Bar({
+            'x':list(pd_pokedex['Name']),
+            'y':list(pd_pokedex[col]),
+            'name':col
+        }) for col in [x for x in pd_pokedex.columns if x != 'Name']
+    ]
+
     layout = {
         'barmode':'group'
     }
 
     fig={'data':traces, 'layout':go.Layout(layout)}
-    return go.Figure(fig) 
+    return go.Figure(fig)
 
 
 
 
 ###### Don't change anything here
-
-
 
 if __name__ == '__main__':
     app.run_server()
